630bba3bd0edf3f97d04f77a4efc26e1b3aa6e6a
Kennyadee1000 <adeniji.kehinde02@gmail.com>
2024-08-21 17:42:03 +0100

    fixing state solution

--- terraform/terraform_backends/dev/backend.tf
+++ terraform/terraform_backends/dev/backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket = "bpty-dev-backend-tfstate"
-    profile = "dev"
-    key = "bpty-state-file/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "bpty-dev-terraform-locks"
-    encrypt = true
-    shared_credentials_files = ["~/.aws/credentials"]
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "bpty-dev-backend-tfstate"
+#    profile = "dev"
+#    key = "bpty-state-file/terraform.tfstate"
+#    region = "us-east-2"
+#    dynamodb_table = "bpty-dev-terraform-locks"
+#    encrypt = true
+#    shared_credentials_files = ["~/.aws/credentials"]
+#  }
+#}
