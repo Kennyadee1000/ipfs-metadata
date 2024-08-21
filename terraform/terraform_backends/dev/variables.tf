094cef41108e7d404bcab8a44b3350544c76424c
Kennyadee1000 <adeniji.kehinde02@gmail.com>
2024-08-21 21:38:16 +0100

    Update backend bucket variable

--- terraform/terraform_backends/dev/variables.tf
+++ terraform/terraform_backends/dev/variables.tf
@@ -1,9 +1,7 @@
 variable "state_bucket_name" {
   description = "The name of the bucket to use as state bucket."
-  default = "bpty-dev-backend-tfstate"
 }
 
 variable "dynamo_lock_table" {
   description = "The name of the table to use as state lock."
-  default = "bpty-dev-terraform-locks"
 }
